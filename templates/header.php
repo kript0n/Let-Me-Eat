cd36dbc6bbd5b84e9246be5dfa5365a660b11271
doomok2100 <brazzers1969@gmail.com>
2014-07-07 03:44:30 +0900

    Created header design

--- templates/header.php
+++ templates/header.php
@@ -10,12 +10,9 @@
     </head>
     
     <header>
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2 col-sm-12" id="my-nav">
+        <div class="container my-nav">
                 <nav class="navbar navbar-default" role="navigation">
-                    <div class="container-fluid">
-                            <div class="col-sm-10 col-sm-offset-1">
-                                <div class="navbar-header col-sm-4">                        
+                                <div class="navbar-header">                        
                                     <a class="navbar-brand">Let Me Eat</a>
                                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                                         <span class="sr-only">Toggle navigation</span>
@@ -24,29 +21,43 @@
                                         <span class="icon-bar"></span>
                                     </button>
                                 </div>
-                                <div class="collapse navbar-collapse col-sm-3">
+                                <div class="collapse navbar-collapse ">
                                     <ul class="nav navbar-nav">
-                                        <li><a href="#">Присоединиться</a></li>
+                                        <li class="active"><a href="<?php echo SITE_URL;?>" class="">Главная</a></li>
+                                        <li><a href="<?php echo (SITE_URL+"index.php?page=connect")?>">Присоединиться</a></li>
                                         <li class="hidden-md hidden-sm hidden-xs"><a href="#"></a></li>
+                                        <li class="hidden-md hidden-sm hidden-xs"><a href="#"></a></li>
+                                        <li class="hidden-md hidden-sm hidden-xs"><a href="#"></a></li>
+                                        <li class="hidden-md hidden-sm hidden-xs"><a href="#"></a></li>
+                                        <li class="hidden-md hidden-sm hidden-xs"><a href="#"></a></li>
+                                        <li class="hidden-md hidden-sm hidden-xs"><a href="#"></a></li>
+                                        
                                         <li>
-                                            <form class="navbar-form">
-                                                 <div class="form-group">
-                                                    <input type="text" class="form-control" placeholder="Search">
+                                            <form class="navbar-form" role="search">
+                                                <div class="form-group name-search"> 
+                                                    <div class="input-group">
+                                                        <input type="search" class="form-control" placeholder="Поиск по названию">
+                                                        <span class="input-group-btn">
+                                                            <button class="btn btn-default" type="submit">
+                                                                <span class="glyphicon glyphicon-search"></span>
+                                                            </button>
+                                                        </span>
+                                                    </div>
                                                 </div>
                                             </form>
                                         </li>
                                        
                                         <li>
-                                            <a href="#" class="col-sm-1 text-center">
-                                                City
-                                             </a>
+                                             <div class="btn-group">
+                                                <button class="btn btn-default navbar-btn city-check dropdown-toggle" type="button" data-toggle="dropdown">
+                                                    <span>Иркутск</span>
+                                                    <span class="caret"></span>
+                                                </button>
+                                            </div>
                                         </li>
                                     </ul>
-                                </div>
-                            </div>
-                    </div>       
+                                </div>   
                 </nav>
-            </div>
         </div>
         
     </header>
