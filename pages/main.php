4bdbbf6142a3ff95a084961f146bec40551ddf54
doomok2100 <brazzers1969@gmail.com>
2014-07-07 17:34:34 +0900

    Created kitchen types checking

--- pages/main.php
+++ pages/main.php
@@ -2,25 +2,66 @@
 
 <div class="container">
     
-    <div class="review-carousel-block">
-        <div id="review-carousel" class="carousel slide" data-ride="carousel" data-interval="3000">
-            <!--Indicators-->
-            <ol class="carousel-indicators">
-                <li data-target="#review-carousel" data-slide-to="0" class="active"></li>
-                <li data-target="#review-carousel" data-slide-to="1"></li>
-                <li data-target="#review-carousel" data-slide-to="2"></li>
-            </ol>
-            
-            <!--Carousel inner-->
-            <div class="carousel-inner">
-                <div class="item active">
-                    <img src="./images/review-carousel/rest1">
-                </div>
-                <div class="item">
-                    <img src="./images/review-carousel/rest2">
+    <div class="main-review-block">
+        
+        <div class="review-carousel-block">
+            <div id="review-carousel" class="carousel slide" data-ride="carousel" data-interval="3000">
+                <!--Indicators-->
+                <ol class="carousel-indicators">
+                    <li data-target="#review-carousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#review-carousel" data-slide-to="1"></li>
+                    <li data-target="#review-carousel" data-slide-to="2"></li>
+                </ol>
+
+                <!--Carousel inner-->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="./images/review-carousel/rest1">
+                    </div>
+                    <div class="item">
+                        <img src="./images/review-carousel/rest2">
+                    </div>
                 </div>
             </div>
         </div>
+        
+        <div class="kitchen-padding">
+            <div class="kitchen">
+                
+                <div class="input-group">
+                    <span class="input-group-addon kitchen-addon">
+                        <div class="review-checkbox">
+                            <input type="checkbox" id="review-checkbox-1">
+                            <label for="review-checkbox-1"></label>
+                        </div>
+                    </span>
+                    <button class="my-btn"><h2>Русская</h2></button>
+                </div>  
+
+                <div class="input-group">
+                    <span class="input-group-addon kitchen-addon no-border-top">
+                        <div class="review-checkbox">
+                            <input type="checkbox" id="review-checkbox-2">
+                            <label for="review-checkbox-2"></label>
+                        </div>
+                    </span>
+                    <button class="my-btn no-border-top"><h2>Русская</h2></button>
+                </div> 
+
+                <div class="input-group">
+                    <span class="input-group-addon kitchen-addon no-border-top">
+                        <div class="review-checkbox">
+                            <input type="checkbox" id="review-checkbox-3">
+                            <label for="review-checkbox-3"></label>
+                        </div>
+                    </span>
+                    <button class="my-btn no-border-top"><h2>Русская</h2></button>
+                </div> 
+                
+                
+            </div>
+        </div>
+      
     </div>
     
 </div>
